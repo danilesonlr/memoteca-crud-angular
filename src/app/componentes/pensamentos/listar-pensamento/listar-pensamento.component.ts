69a9ae97133d97c677cd395d07e85fbab30d4646
danielson.ramos <danielson.ramos@valecard.onmicrosoft.com>
2024-07-23 11:23:26 -0300

    Aula 03 - Implementando a paginação.

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,12 +11,24 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
 
+  paginaAtual: number = 1;
+  haMaispensamentos: boolean = true;
+
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     });
+  }
+
+  carregarMaisPensamentos(){
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos =>{
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length){
+        this.haMaispensamentos = false;
+      }
+    })
   }
 
 }
